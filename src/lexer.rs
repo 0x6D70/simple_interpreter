f0836cd26cf040b1f8239d222bdc0968a335a9f5
0x6D70 <pfeifenberger.markus2003@gmail.com>
2022-07-30 09:57:08 +0200

    part 1

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,8 +1,9 @@
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum TokenType {
     Integer(isize),
     Plus,
+    Minus,
     Eof
 }
 
@@ -16,7 +17,7 @@ pub struct Interpreter<'a> {
 impl<'a> Interpreter<'a> {
     pub fn new(text: &str) -> Interpreter {
         Interpreter {
-            text: text,
+            text,
             pos: 0,
             current_token: TokenType::Eof,
         }
@@ -26,6 +27,20 @@ impl<'a> Interpreter<'a> {
         panic!("Error parsing input: {:#?}", self);
     }
 
+    fn integer(&mut self) -> TokenType {
+        let start_index = self.pos;
+
+        while let Some(c) = self.text.chars().nth(self.pos) {
+            if !c.is_ascii_digit() {
+                break;
+            }
+
+            self.pos += 1;
+        }
+
+        TokenType::Integer(self.text[start_index..self.pos].parse::<isize>().unwrap())
+    }
+
     fn get_next_token(&mut self) -> TokenType {
         if self.pos >= self.text.len() {
             return TokenType::Eof;
@@ -33,14 +48,23 @@ impl<'a> Interpreter<'a> {
 
         let current_char = self.text.chars().nth(self.pos).unwrap();
 
-        if current_char.is_digit(10) {
+        if current_char.is_ascii_whitespace() {
             self.pos += 1;
-            return TokenType::Integer(current_char.to_digit(10).unwrap() as isize);
+            return self.get_next_token();
+        }
+
+        if current_char.is_ascii_digit() {
+            return self.integer();
         }
 
         if current_char == '+' {
             self.pos += 1;
             return TokenType::Plus;
+        }
+
+        if current_char == '-' {
+            self.pos += 1;
+            return TokenType::Minus;
         }
 
         self.error()
@@ -65,9 +89,13 @@ impl<'a> Interpreter<'a> {
 
         self.eat(TokenType::Integer(left));
 
-        let op = &self.current_token;
+        let op = self.current_token;
 
-        self.eat(TokenType::Plus);
+        if op == TokenType::Plus {
+            self.eat(TokenType::Plus);
+        } else {
+            self.eat(TokenType::Minus);
+        }
 
         let right = match self.current_token {
             TokenType::Integer(value) => value,
@@ -76,9 +104,11 @@ impl<'a> Interpreter<'a> {
 
         self.eat(TokenType::Integer(right));
 
-        let result = left + right;
-
-        result
+        match op {
+            TokenType::Plus => left + right,
+            TokenType::Minus => left - right,
+            _ => self.error()
+        }
     }
 
 }
