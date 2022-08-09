735f655af4970a59c5f523061f4728ec25d02db0
Markus Pfeifenberger <pfeifenberger.markus2003@gmail.com>
2022-08-09 18:58:56 +0200

    add transpiler to rpn and lisp

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -19,3 +19,56 @@ pub fn evaluate(expr: Expr) -> isize {
         Expr::Int(value) => value,
     }
 }
+
+#[allow(dead_code)]
+pub fn evaluate_rpn(expr: Expr) {
+    match expr {
+        Expr::Binary { left, op, right } => {
+            evaluate_rpn(*left);
+            evaluate_rpn(*right);
+
+            match op {
+                TokenType::Plus => print!(" +"),
+                TokenType::Minus => print!(" -"),
+                TokenType::Star => print!(" *"),
+                TokenType::Slash => print!(" /"),
+                TokenType::Power => print!(" ^"),
+                _ => unreachable!(),
+            }
+        }
+        Expr::Grouping(expr_grouping) => {
+            evaluate_rpn(*expr_grouping);
+        }
+        Expr::Int(value) => {
+            print!(" {}", value);
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn evaluate_lisp(expr: Expr) {
+    match expr {
+        Expr::Binary { left, op, right } => {
+            print!("(");
+            match op {
+                TokenType::Plus => print!(" +"),
+                TokenType::Minus => print!(" -"),
+                TokenType::Star => print!(" *"),
+                TokenType::Slash => print!(" /"),
+                TokenType::Power => print!(" ^"),
+                _ => unreachable!(),
+            }
+
+            evaluate_lisp(*left);
+            evaluate_lisp(*right);
+
+            print!(")");
+        }
+        Expr::Grouping(expr_grouping) => {
+            evaluate_lisp(*expr_grouping);
+        }
+        Expr::Int(value) => {
+            print!(" {}", value);
+        }
+    }
+}
